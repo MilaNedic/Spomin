574c381beed72dd1fd5e517d5e22959f98e3c98b
MilaNedic <49908385+MilaNedic@users.noreply.github.com>
2019-07-15 16:43:04 +0200

    Add files via upload
    model igre spomin

---
+++ model.py
@@ -0,0 +1,16 @@
+import random
+import json
+
+# Definirajmo konstante
+VELIKOST_IGRALNE_PLOSCE = 24
+
+ZACETEK = 'S'
+ZMAGA = 'W'
+PORAZ = 'L'
+
+# Definirajmo logicni model igre
+
+class Spomin:
+
+    def __init__(self, kvak): # se ne vemo tocno, kaj bi delali
+        self.kvak = 'kvak'
